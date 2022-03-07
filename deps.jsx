ae133c1c592b61b07cd4287de95cff22b5175778
Viet Tran <viet.2.tran@aalto.fi>
2022-03-07 22:04:18 +0200

    Added a cat page

--- deps.jsx
+++ deps.jsx
@@ -1,6 +1,6 @@
 export { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
 export { viewEngine, engineFactory, adapterFactory } from "https://deno.land/x/view_engine@v1.5.0/mod.ts";
-export { Client } from "https://deno.land/x/postgres@v0.10.0/mod.ts";
+//export { Client } from "https://deno.land/x/postgres@v0.10.0/mod.ts";
 //export { Client } from "https://deno.land/x/mysql/mod.ts";
 //export * as bcrypt from "https://deno.land/x/bcrypt@v0.2.4/mod.ts";
 export { Session } from "https://deno.land/x/session@v1.0.0/mod.ts";
