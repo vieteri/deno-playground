6b6488430c6e817336e22dd500563abacd3ae4fd
Viet Tran <viet.2.tran@aalto.fi>
2022-03-07 22:15:57 +0200

    Added a cat page

--- app.tsx
+++ app.tsx
@@ -1,23 +1,20 @@
 import { Application } from "./deps.jsx";
-import { viewEngine, engineFactory, adapterFactory, Session } from "./deps.jsx";
+import { Session } from "./deps.jsx";
 import { router } from "./routes/routes.jsx";
 
 const app = new Application();
 const session = new Session({ framework: "oak" });
 await session.init();
 app.use(session.use()(session));
-
+/*
 const ejsEngine = engineFactory.getEjsEngine();
 const oakAdapter = adapterFactory.getOakAdapter();
 app.use(viewEngine(oakAdapter, ejsEngine, {viewRoot: "./views"}));
-
+*/
 
 app.use(router.routes());
 
-if (!Deno.env.get('TEST_ENVIRONMENT')) {
-  app.listen({ port: 8000 });
-}
-else {
-  app.listen({ port: 8000 });
-}
+
+app.listen({ port: 8000 });
+
 export default app;
