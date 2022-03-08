c95caa144099148e6116c4ee069a875ca25ba5fa
vieteri <viet_tran@windowslive.com>
2022-03-08 14:25:21 +0200

    fixed holes

--- app.tsx
+++ app.tsx
@@ -1,16 +1,16 @@
 import { Application } from "./deps.jsx";
-import { Session } from "./deps.jsx";
+import { viewEngine, engineFactory, adapterFactory, Session } from "./deps.jsx";
 import { router } from "./routes/routes.jsx";
 
 const app = new Application();
 const session = new Session({ framework: "oak" });
 await session.init();
 app.use(session.use()(session));
-/*
+
 const ejsEngine = engineFactory.getEjsEngine();
 const oakAdapter = adapterFactory.getOakAdapter();
 app.use(viewEngine(oakAdapter, ejsEngine, {viewRoot: "./views"}));
-*/
+
 
 app.use(router.routes());
 
