bf556ff706ff5dae3a548e94a6855b0f13444905
Viet Tran <viet.tran@softsys.fi>
2022-03-08 16:11:03 +0200

    change to tsx

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: deno run --allow-all --unstable heroku-deploy.js ${PORT}
+web: deno run --allow-all --unstable heroku-deploy.tsx ${PORT}
--- heroku-deploy.js
+++
@@ -1,9 +0,0 @@
-import { app } from "./app.tsx";
-
-let port = 7777;
-if (Deno.args.length > 0) {
-  const lastArgument = Deno.args[Deno.args.length - 1];
-  port = Number(lastArgument);
-}
-
-app.listen({ port: port });
