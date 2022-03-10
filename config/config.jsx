5af3ec1ebecababe012e91a9fdd2c93fbea035b7
Viet Tran <viet.2.tran@aalto.fi>
2022-03-10 15:55:04 +0200

    added config

--- config/config.jsx
+++ config/config.jsx
@@ -2,10 +2,16 @@ let config = {};
 
 if (Deno.env.get('TEST_ENVIRONMENT')) {
   config.database = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    } 
+    
+      user: process.env.POSTGRES_USER,
+      host: process.env.POSTGRES_HOST,
+      database: process.env.POSTGRES_DB,
+      password: process.env.POSTGRES_PASSWORD,
+      port: process.env.POSTGRES_PORT,
+      ssl: {
+          rejectUnauthorized: false,
+      }
+  
   }
 
 } else {
